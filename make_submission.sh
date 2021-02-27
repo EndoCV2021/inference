00de2ff46baa82efadf9c5e9f2e15644c37eff85
Hyunseok, Lee <hyunseoki@kaist.ac.kr>
2021-02-27 20:39:25 +0900

    Update make_submission.sh

--- make_submission.sh
+++ make_submission.sh
@@ -3,6 +3,7 @@ source /home/dgmif/workspace/venv/endocv2021/bin/activate
 
 python inference.py --cfg_path ./model/Mask_RCNN_Res50_FPN_3x.yaml \
                      --model_path ./model/model_0040999.pth \
-                     --image_path ./sample_data
+                     --image_path ./sample_data \
+                     --device cuda:0 
 
 deactivate
