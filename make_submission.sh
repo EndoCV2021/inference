b488128345ec910ffcdeda4bb0dd35539acb89e4
hyunseoki <hyunseoki@kaist.ac.kr>
2021-02-28 17:17:43 +0900

    chagne config to output/config.yml

--- make_submission.sh
+++ make_submission.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 source /home/dgmif/workspace/venv/endocv2021/bin/activate
 
-python inference.py --cfg_path ./model/Mask_RCNN_Res50_FPN_3x.yaml \
+python inference.py --cfg_path ./model/config.yaml \
                      --model_path ./model/model_0040999.pth \
                      --image_path ./sample_data \
                      --device cuda:0 
