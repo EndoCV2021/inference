07b64a22ac35e8f6fede257e80e27153acf100c5
hyunseoki <hyunseoki@kaist.ac.kr>
2021-03-03 22:42:34 +0900

    제출 형식으로 출력물 압축 되게 수정

--- make_submission.sh
+++ make_submission.sh
@@ -1,9 +1,20 @@
 #!/bin/bash
-source /home/dgmif/workspace/venv/endocv2021/bin/activate
+# source /Users/hyunseoki/workspace/venv/endocv2021/bin/activate
 
-python inference.py --cfg_path ./model/config.yaml \
-                     --model_path ./model/model_0040999.pth \
-                     --image_path ./sample_data \
-                     --device cuda:0 
+# python inference.py --cfg_path ./model/5.yaml \
+#                      --model_path ./model/5.pth \
+#                      --image_path ./sample_data \
+#                      --device cpu 
 
-deactivate
+# deactivate
+
+mkdir EndoCV2021_detection
+mkdir EndoCV2021_segmentation
+
+mv ./EndoCV2021/detection ./EndoCV2021_detection
+mv ./EndoCV2021/segmentation ./EndoCV2021_segmentation
+
+zip -r EndoCV2021_detection.zip ./EndoCV2021_detection
+zip -r EndoCV2021_segmentation.zip ./EndoCV2021_segmentation
+
+rm -rf EndoCV2021 EndoCV2021_detection EndoCV2021_segmentation
