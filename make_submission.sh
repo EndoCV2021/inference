e32c05c2a0a12499abd84cd01f9bb9fdd6837d88
hyunseoki <hyunseoki@kaist.ac.kr>
2021-03-04 23:50:09 +0900

    update

--- make_submission.sh
+++ make_submission.sh
@@ -1,20 +1,26 @@
-#!/bin/bash
-# source /Users/hyunseoki/workspace/venv/endocv2021/bin/activate
+source /Users/hyunseoki/workspace/venv/endocv2021/bin/activate                                              Modified
+# source ../env01/bin/activate
+pip install -r requirements.txt > /dev/null
 
-# python inference.py --cfg_path ./model/5.yaml \
-#                      --model_path ./model/5.pth \
-#                      --image_path ./sample_data \
-#                      --device cpu 
+echo ======= Detection Start =======
 
-# deactivate
+python -W ignore inference_ensemble_detection.py \
+                     --cfg_dir ensemble_weights/segmentation \
+                     --model_dir ensemble_weights/segmentation \
+                     --image_path ./sample_data \
+                     --device cpu 
 
-mkdir EndoCV2021_detection
-mkdir EndoCV2021_segmentation
+zip -r result_detection.zip EndoCV2021
+rm -rf EndoCV2021
 
-mv ./EndoCV2021/detection ./EndoCV2021_detection
-mv ./EndoCV2021/segmentation ./EndoCV2021_segmentation
+echo ======= Segmentation Start =======
+python -W ignore inference_ensemble_segmentation.py \
+                     --cfg_dir ensemble_weights/detection \
+                     --model_dir ensemble_weights/detection \
+                     --image_path ./sample_data \
+                     --device cpu 
 
-zip -r EndoCV2021_detection.zip ./EndoCV2021_detection
-zip -r EndoCV2021_segmentation.zip ./EndoCV2021_segmentation
+zip -r result_segmentation.zip EndoCV2021
+rm -rf EndoCV2021
 
-rm -rf EndoCV2021 EndoCV2021_detection EndoCV2021_segmentation
+deactivate
